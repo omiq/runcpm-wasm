f6283722fb10908b3e9d4387692a1be78b9b4e6b
Chris Garrett <chris@chrisg.com>
2026-07-12 13:28:21 +0100

    cpm ascii logo

--- cpm-programs/demos/welcome.c
+++ cpm-programs/demos/welcome.c
@@ -9,6 +9,16 @@ int main(void) {
     /* VT100 clear + home. z88dk's clrscr() emits a code RunCPM's terminal
        doesn't map, so drive the terminal directly (ESC[2J clears, ESC[H homes). */
     printf("\x1b[2J\x1b[H");
-    printf("WELCOME TO CP/M!\r\n");
+    
+    
+    printf("STARTING CP/M ...\r\n\r\n");
+
+    printf( "   ____ ____   ____  __ \r\n");
+    printf( "  / ___|  _ \\ / /  \\/  |\r\n");
+    printf( " | |   | |_) / /| |\\/| |\r\n");
+    printf( " | |___|  __/ / | |  | |\r\n");
+    printf( "  \\____|_| /_/  |_|  |_|\r\n");
+    printf( "                        \r\n\r\n");
+
     return 0;
 }
